c226eaf34aabce629e916769ef633ef52cf4869a
amazingefren <efrentheroker@hotmail.com>
2020-09-16 17:38:00 -0600

    cookie authentication, register sets cookie

--- server/src/resolvers/user-resolver.ts
+++ server/src/resolvers/user-resolver.ts
@@ -6,6 +6,7 @@ import {
   InputType,
   Mutation,
   ObjectType,
+  Query,
   Resolver,
 } from "type-graphql";
 import { User } from "../entities/User";
@@ -37,10 +38,20 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    if (!req.session!.userId) {
+      return null;
+    }
+
+    const user = await em.findOne(User, { id: req.session!.userId });
+    return user;
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -77,6 +88,9 @@ export class UserResolver {
         };
       }
     }
+
+    req.session!.userId = user.id;
+
     return { user };
   }
 
