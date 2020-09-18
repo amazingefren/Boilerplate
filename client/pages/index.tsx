6d536c0f5fd9633a4924428505364d2a65cf9508
amazingefren <efrentheroker@hotmail.com>
2020-09-17 18:12:27 -0600

    temporary login and register buttons on home page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,18 @@
 //import Link from "next/link";
 import React from "react";
 import styles from "./modules/index.module.scss";
+import Link from "next/link";
 
 const IndexPage = () => (
   <div id={styles.indexWrapper}>
     <h1>Hello World</h1>
     <h2>Boilerplate</h2>
+    <Link href="/register">
+      <a>Register</a>
+    </Link>
+    <Link href="/login">
+      <a>Login</a>
+    </Link>
   </div>
 );
 
